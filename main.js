58852b5ea758a6bd6a9f2311603f5877680a7967
Zahar Andrienko <zaharandrienko88@gmail.com>
2021-07-21 11:58:10 +0300

    styles

---
+++ main.js
@@ -0,0 +1,8 @@
+function serchOn() {
+    var serch = document.getElementsByClassName("none");
+    var i;
+    for (i = 0; i < serch.length; i++) {
+        serch[i].style.display = "block";
+    }
+}
+
